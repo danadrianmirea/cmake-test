8509f4c402e110ea308c478ec0f8b54ada27d5ca
Dan Adrian Mirea <damirea84@gmail.com>
2024-10-15 12:37:27 +0300

    remove init.bat

--- init.bat
+++
@@ -1,7 +0,0 @@
-echo "# cmake-test" >> README.md
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:danadrianmirea/cmake-test.git
-git push -u origin main
