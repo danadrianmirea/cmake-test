883c68c978f4c0b2689a5467b395d5ab925ea055
Dan Adrian Mirea <damirea84@gmail.com>
2024-10-16 09:13:05 +0300

    fix cmakelists.txt error resulting in double .exe binary

--- code/main.cpp
+++ code/main.cpp
@@ -1,8 +1,24 @@
 #include <cstdint>
 #include <iostream>
+#include <vector>
+
+void printVec(std::vector<int> &v)
+{
+    std::cout << std::endl;
+    for (auto e : v)
+    {
+        std::cout << "Value is: " << e << std::endl;
+    }
+    std::cout << std::endl;
+}
 
 int32_t main()
 {
-    std::cout << "Hello world using cmake\n";
+    std::vector<int> v = {3, 4, 8, 2};
+
+    printVec(v);
+    v.push_back(18);
+    printVec(v);
+
     return 0;
 }
